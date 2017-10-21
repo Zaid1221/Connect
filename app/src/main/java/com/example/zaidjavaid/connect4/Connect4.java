c153eabdb6662f91735a992db1dec997412ebb6f
Zaid Javaid <zaid.javaid@gmail.com>
2017-10-21 16:31:05 -0400

    Winning the game now works

--- app/src/main/java/com/example/zaidjavaid/connect4/Connect4.java
+++ app/src/main/java/com/example/zaidjavaid/connect4/Connect4.java
@@ -44,230 +44,40 @@ public class Connect4 {
         return -1;
     }
 
-    public int whoWon()
-    {
-        int row = checkRows();
-        if(row > 0)
-        {
-            return row;
-        }
-        int col = checkColumn();
-        if(col > 0)
-        {
-            return col;
-        }
-        int diag = checkDiaganol2();
-        if(diag > 0)
-        {
-            return diag;
-        }
-        return 0;
-    }
+    public static int checkWin(int[][] board) {
+        final int HEIGHT = board.length;
+        final int WIDTH = board[0].length;
+        final int EMPTY_SLOT = 0;
+        for (int r = 0; r < HEIGHT; r++) { // iterate rows, bottom to top
+            for (int c = 0; c < WIDTH; c++) { // iterate columns, left to right
+                int player = board[r][c];
+                if (player == EMPTY_SLOT)
+                    continue; // don't check empty slots
 
-    protected int checkRows()
-    {
-        for(int row=0; row < Rows; row++)
-        {
-            int temp = 0;
-            int col = 0;
-            int player = game[col][row]; //gets the first spot of that row
-            col++;
-            while(col<7)
-            {
-                if(game[row][col] == player && game[row][col] != 0)
-                {
-                    temp++;
-                }
-                else
-                {
-                    temp = 0;
-                    game[row][col] = player;
-                }
-                if(temp == 4)
-                {
-                    break;
-                }
-                col++;
-            }
-            if(temp == 4)
-            {
-                return game[row][col-1];
-            }
-        }
-        return 0;
-    }
-
-    protected int checkColumn()
-    {
-        for(int col = 0; col < Columns; col++)
-        {
-            int temp = 0;
-            int row = 0;
-            int player = game[row][col];
-            row++;
-            while(row<6)
-            {
-                if(game[row][col] == player && game[row][col] != 0)
-                {
-                    temp++;
-                }
-                else
-                {
-                    temp = 0;
-                    player = game[row][col];
-                }
-                if(temp == 4)
-                {
-                    break;
-                }
-                row++;
-            }
-            if(temp == 4)
-            {
-                return game[row-1][col];
-            }
-        }
-        return 0;
-    }
-
-   protected int checkDiaganol2() {
-       int row;
-       int col;
-       int player = 0;
-       int endpoint = 6;
-       int temp = 0;
-
-       //First check all diaganols from top left ot bottom right
-
-       for (row = 0, col = 0; col < 4; col++, endpoint--) //This just checks the first row diagnols
-       {
-           if (col == 1) // when hit the second column on first row keep the endpoint 6, length of diagnol check is still 6
-           {
-               endpoint = 6;
-           }
-           while (endpoint > 0) {
-               game[row][col] = player;
-               if (game[++row][++col] == player && game[row][col] != 0) {
-                   temp++;
-               } else {
-                   temp = 0;
-                   player = game[row][col];
-               }
-               if (temp == 4) {
-                   return player;
-               }
-           }
-       }
-
-       /*
-       for (endpoint = 5, col = 0, row = 1; row < 3; row++, endpoint--) //This checks the second, third row first column for diaganols
-       {
-           while (endpoint > 0) {
-               game[row][col] = player;
-               if (game[++row][++col] == player && game[row][col] != 0) {
-                   temp++;
-               } else {
-                   temp = 0;
-                   player = game[row][col];
-               }
-               if (temp == 4) {
-                   return player;
-               }
-           }
-       }
-       */
-       return 0;
-   }
-
-
-    protected int checkDiaganol()
-    {
-        int rowT; //temporary row varaible used to rememeber where row is
-        int colT; //temporary column varaible used to remember where column
-        int temp = 0;
-        int endpoint; //how far you need to check down the diaganol lane
-        int Tendpoint;
-        int player;
-
-        for(int row=0; row<3; row++) //this loop handles all diaganol wins from top left to bottom right
-        {
-            endpoint = 6;
-
-            if(row == 1)
-            {
-                endpoint = 6;
-            }
-
-            for (int col = 0; col < 4; col++, endpoint--)
-            {
-
-                if(col == 1)
-                {
-                    endpoint = 6;
-                }
-
-                rowT = row;
-                colT = col;
-                player = game[rowT][colT];
-                Tendpoint = endpoint;
-                while (Tendpoint > 0)
-                {
-                    if ((game[++rowT][++colT] == player) && (game[rowT][colT] != 0))
-                    {
-                        temp++;
-                    } else
-                    {
-                        temp = 0;
-                        player = game[rowT][colT];
-                    }
-                    if(temp == 4)
-                    {
-                        break;
-                    }
-                    Tendpoint--;
-                }
-                if(temp == 4)
-                {
-                    return game[rowT][colT];
+                if (c + 3 < WIDTH &&
+                        player == board[r][c+1] && // look right
+                        player == board[r][c+2] &&
+                        player == board[r][c+3])
+                    return player;
+                if (r + 3 < HEIGHT) {
+                    if (player == board[r+1][c] && // look up
+                            player == board[r+2][c] &&
+                            player == board[r+3][c])
+                        return player;
+                    if (c + 3 < WIDTH &&
+                            player == board[r+1][c+1] && // look up & right
+                            player == board[r+2][c+2] &&
+                            player == board[r+3][c+3])
+                        return player;
+                    if (c - 3 >= 0 &&
+                            player == board[r+1][c-1] && // look up & left
+                            player == board[r+2][c-2] &&
+                            player == board[r+3][c-3])
+                        return player;
                 }
             }
         }
-
-        for(int row=5; row>3; row--) //this loop will handle all diaganols wins from bottom left to top right
-        {
-            endpoint = 6;
-
-            if(row == 4)
-            {
-                endpoint = 5;
-            }
-
-            rowT = row;
-            for(int col = 0; col <4; col++,endpoint--)
-            {
-                colT = col;
-                player = game[row][col];
-                while(temp <4 || endpoint!=0)
-                {
-                    if (game[row--][col++] == player)
-                    {
-                        temp++;
-                    }
-                    else
-                    {
-                        temp = 0;
-                        player = game[row][col];
-                    }
-                }
-                if(temp == 4)
-                {
-                    return game[row][col];
-                }
-                col = colT;
-                row = rowT;
-            }
-        }
-        return 0;
+        return EMPTY_SLOT; // no winner found
     }
 
     public boolean Fill(int col) //checks if the column is filled
@@ -328,9 +138,8 @@ public class Connect4 {
 
     public boolean isGameOver( )
     {
-        return cannotPlay( ) || ( whoWon( ) > 0 );
+        return cannotPlay( ) || (checkWin(game) > 0 );
     }
-
 
     public void resetGame()
     {
