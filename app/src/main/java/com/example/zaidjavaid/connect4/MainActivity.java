eb715390113d7dc59cdba0ada1f2e1b5263550af
Zaid Javaid <zaid.javaid@gmail.com>
2017-10-22 23:44:17 -0400

    Game is fully playable with play again dialog and exit dialog buttons at the end of game

--- app/src/main/java/com/example/zaidjavaid/connect4/MainActivity.java
+++ app/src/main/java/com/example/zaidjavaid/connect4/MainActivity.java
@@ -15,112 +15,25 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity
 {
     private Connect4 con;
-    private Button [][] buttons;
-    private TextView status;
+    private PlayArea area;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         con = new Connect4();
-        buildGUI();
-    }
 
-    public void buildGUI()
-    {
         // Get width of the screen
         Point size = new Point();
         getWindowManager().getDefaultDisplay().getSize(size);
         int w = size.x / Connect4.Rows;
         int v = size.x / Connect4.Columns;
 
-        // Create the layout manager as a GridLayout
-        GridLayout gridLayout = new GridLayout(this);
-        gridLayout.setColumnCount(Connect4.Columns);
-        gridLayout.setRowCount(Connect4.Rows + 1);
-
-        // Create the buttons and add them to gridLayout
-        buttons = new Button[Connect4.Rows][Connect4.Columns];
         ButtonHandler bh = new ButtonHandler( );
-        for(int row=0; row<Connect4.Rows; row++)
-        {
-            for(int col=0; col<Connect4.Columns; col++)
-            {
-                buttons[row][col] = new Button(this);
-                buttons[row][col].setTextSize( ( int ) ( v * .2 ) );
-                buttons[row][col].setOnClickListener( bh );
-                gridLayout.addView(buttons[row][col],v,w);
-            }
-        }
-
-
-        //set up layout parameters of 7th row of layout for status bar
-        status = new TextView(this);
-        GridLayout.Spec rowSpec = GridLayout.spec(Connect4.Rows, 1);
-        GridLayout.Spec columnSpec = GridLayout.spec(0, Connect4.Columns);
-        GridLayout.LayoutParams lpStatus = new GridLayout.LayoutParams(rowSpec, columnSpec);
-        status.setLayoutParams(lpStatus);
-
-
-        //set status height and width
-        status.setWidth(Connect4.Columns * w);
-        status.setHeight(v);
-        status.setGravity(Gravity.CENTER);
-        status.setBackgroundColor(Color.GREEN);
-        status.setTextSize((int) (w*.15));
-        status.setText(con.result());
-        gridLayout.addView(status);
-
-
-
+        area = new PlayArea(this, w, v, bh);
+        area.setStatusText(con.result());
         // Set gridLayout as the View of this Activity
-        setContentView( gridLayout );
-    }
-
-    public void update(int col) //This does the actual visual setting of text in game board
-    {
-        int row = con.Test(col); //gets the value of row, checks column is in play area ansd if column is full
-        if(row != -1)
-        {
-            int play = con.play(row, col);
-            if (play == 1)
-            {
-                buttons[row][col].setText("X");
-            }
-            else if (play == 2)
-            {
-                buttons[row][col].setText("O");
-            }
-            if (con.isGameOver())
-            {
-                status.setBackgroundColor(Color.RED);
-                enableButtons(false);
-                status.setText(con.result());
-                showNewGameDialog();
-            }
-        }
-    }
-
-    public void enableButtons( boolean enabled )
-    {
-        for( int row = 0; row < Connect4.Rows; row++ )
-        {
-            for (int col = 0; col < Connect4.Columns; col++)
-            {
-                buttons[row][col].setEnabled(enabled);
-            }
-        }
-    }
-
-    public void resetButtons()
-    {
-        for(int row=0; row < Connect4.Rows; row++)
-        {
-            for(int col=0; col < Connect4.Columns; col++)
-            {
-                buttons[row][col].setText("");
-            }
-        }
+        setContentView(area);
     }
 
     public void showNewGameDialog()
@@ -143,9 +56,28 @@ public class MainActivity extends AppCompatActivity
             {
                 for (int column = 0; column < Connect4.Columns; column++)
                 {
-                    if (v == buttons[row][column])
+                   if(area.isButton((Button)v, row, column))
                     {
-                        update(column);
+                        int Row = con.Test(column); //gets the value of row, checks column is in play area ansd if column is full
+                        if(row != -1)
+                        {
+                            int play = con.play(Row, column);
+                            if (play == 1)
+                            {
+                                area.setButtonText(Row, column, "X");
+                            }
+                            else if (play == 2)
+                            {
+                                area.setButtonText(Row, column, "O");
+                            }
+                            if (con.isGameOver())
+                            {
+                                area.setBackgroundColor(Color.RED);
+                                area.enableButtons(false);
+                                area.setStatusText(con.result());
+                                showNewGameDialog();
+                            }
+                        }
                     }
                 }
             }
@@ -159,10 +91,10 @@ public class MainActivity extends AppCompatActivity
             if(id == -1)
             {
                 con.resetGame();
-                enableButtons(true);
-                resetButtons();
-                status.setBackgroundColor((Color.GREEN));
-                status.setText(con.result());
+                area.enableButtons(true);
+                area.resetButtons();
+                area.setBackgroundColor((Color.GREEN));
+                area.setStatusText(con.result());
             }
             else if(id == -2)
             {
--- app/src/main/java/com/example/zaidjavaid/connect4/PlayArea.java
+++ app/src/main/java/com/example/zaidjavaid/connect4/PlayArea.java
@@ -1,8 +1,106 @@
 package com.example.zaidjavaid.connect4;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.view.Gravity;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.TextView;
+
+import static android.R.attr.width;
+
 /**
  * Created by Zaid Javaid on 10/12/2017.
  */
 
-public class PlayArea {
+public class PlayArea extends GridLayout
+{
+    private int columns;
+    private int rows;
+    private Button[][] buttons;
+    private TextView status;
+
+    public PlayArea(Context context, int newrows, int newcolumns, OnClickListener listener)
+    {
+        super(context);
+        rows = newrows;
+        columns = newcolumns;
+
+        //Set the number of rows and columns in the grid layout
+        setColumnCount(columns);
+        setRowCount(rows + 1);
+
+        //Create the buttons and add them to the layout
+        buttons = new Button[rows][columns];
+        for(int row=0; row<rows; row++)
+        {
+            for(int col=0; col<columns; col++)
+            {
+                buttons[row][col] = new Button(context);
+                buttons[row][col].setTextSize((int)(width * .2));
+                buttons[row][col].setOnClickListener(listener);
+                addView(buttons[row][col], rows, columns);
+            }
+        }
+
+        //Set up the layout of the bottom row for state of the game
+        status = new TextView(context);
+        Spec rowsSpec = GridLayout.spec(rows, 1);
+        Spec columnSpec = GridLayout.spec(0, columns);
+        LayoutParams lpStatus = new LayoutParams(rowsSpec, columnSpec);
+        status.setLayoutParams(lpStatus);
+
+        //set up status bar
+        status.setWidth(rows * columns);
+        status.setHeight(columns);   //***************************if does not look right look into this
+        status.setGravity(Gravity.CENTER);
+        status.setBackgroundColor(Color.GREEN);
+        status.setTextSize((int)(rows * .15));
+        addView(status);
+    }
+
+    public void setStatusText(String text)
+    {
+        status.setText(text);
+    }
+
+    public void setBackgroundColor(int color)
+    {
+        status.setBackgroundColor(color);
+    }
+
+    public void setButtonText(int row, int column, String text)
+    {
+        buttons[row][column].setText(text);
+    }
+
+    public boolean isButton(Button b, int row, int column)
+    {
+        return (b == buttons[row][column]);
+    }
+
+    public void resetButtons()
+    {
+        for(int row=0; row<rows; row++)
+        {
+            for(int col=0; col<columns; col++)
+            {
+                buttons[row][col].setText("");
+            }
+        }
+    }
+
+    public void enableButtons(boolean enabled)
+    {
+        for(int row=0; row<rows; row++)
+        {
+            for(int col=0; col<columns; col++)
+            {
+                buttons[row][col].setEnabled(enabled);
+            }
+        }
+    }
+
+
 }
